6c6dbbfafc0bc41eb7b0eabf55138bf656a32599
nag763 <loic.labeye@tutanota.de>
2020-12-04 09:59:26 +0100

    Vanilla

---
+++ app/src/main/java/com/ticandroid/baley_labeye/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ticandroid.baley_labeye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
