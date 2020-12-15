2961a037e170282c05516c521df0f6e3915d6a0d
nag763 <loic.labeye@tutanota.de>
2020-12-15 12:04:40 +0100

    Documentate and prettify code

--- app/src/main/java/com/ticandroid/baley_labeye/activities/MuseumReaderActivity.java
+++ app/src/main/java/com/ticandroid/baley_labeye/activities/MuseumReaderActivity.java
@@ -9,6 +9,8 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,7 +38,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
@@ -44,31 +45,71 @@ import java.util.TimeZone;
 
 import static java.text.DateFormat.getDateTimeInstance;
 
+/**
+ * This activity's purpose is to display
+ * elegantly the different informations
+ * related to a museum.
+ *
+ * @author Baley
+ * @author Labeye
+ * @see AppCompatActivity
+ */
 public class MuseumReaderActivity extends AppCompatActivity {
 
+    /** Firestore instance **/
     private transient final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-
+    /** Ressources of the application **/
     private transient Resources res;
+    /** Permissions needed in this app **/
+    private final static String[] PERMISSIONS_NEEDED =
+            {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            };
+    /** Location manager used to get the user's position **/
+    private transient LocationManager mLocationManager;
+    /** Refresh button to refresh our position **/
+    private transient Button refreshButton;
+    /** Museum bean fetched from remote firestore instance **/
+    private transient MuseumBean museumBean;
 
-
-    private final static String[] PERMISSIONS_NEEDED = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION };
-
-    private LocationManager mLocationManager;
-
-
-    private transient static MuseumBean museumBean;
-
-    // TODO : WAIT FOR LOCATION
+    /**
+     *  Location listener used to get our user position
+     */
     private final LocationListener mLocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(final Location location) {
             String userPosition = String.format("%s,%s", location.getLongitude(), location.getLatitude());
             Log.d(this.getClass().getName(), String.format("Location changed to %s", userPosition));
-            // TODO : ONCE GOTTEN, STOP LISTENER
+            // Once the location is fetched, there is no need for new updates
             mLocationManager.removeUpdates(this);
             bindDistanceToMuseum(userPosition, museumBean);
         }
     };
+
+    /**
+     *  Method to access the user's location
+     */
+    private void accessLocation() {
+        String textToDisplay = res.getString(R.string.location_service_disabled);
+        Log.d(this.getClass().getName(), "Location accesser called");
+        // If the user hasn't accepted the usage of its location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(this.getClass().getName(), "Location permission hasn't been accepted on user's device");
+            requestPermissions(PERMISSIONS_NEEDED, 124);
+        // If the location hasn't been enabled on the user's device
+        } else if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                && !mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            Log.i(this.getClass().getName(), "Location is disabled on user's device");
+        // If the location is enabled on user's device
+        } else {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, mLocationListener);
+            textToDisplay = res.getString(R.string.waiting_for_service);
+            refreshButton.setVisibility(View.GONE);
+        }
+        ((TextView) findViewById(R.id.tvDistanceToMuseum)).setText(textToDisplay);
+    }
 
 
     @Override
@@ -78,39 +119,26 @@ public class MuseumReaderActivity extends AppCompatActivity {
 
         res = getResources();
 
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        // TODO : ASSERT THAT IT REQUESTS ONLY ONCE
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(PERMISSIONS_NEEDED, 124);
-            return;
-        }
-        if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || !mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            ((TextView) findViewById(R.id.tvDistanceToMuseum)).setText(res.getString(R.string.location_service_disabled));
-            Log.d(this.getClass().getName(), "Location is disabled on user's device");
-        } else {
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, mLocationListener);
-        }
-
+        // Fetching our document
         final String DOCUMENT_ID = getIntent().getStringExtra(MuseumListFSAdapter.KEY_OF_EXTRA);
         DocumentReference museumDocument = firebaseFirestore.collection(res.getString(R.string.fs_museums)).document(DOCUMENT_ID);
-
         Task<DocumentSnapshot> task = museumDocument.get();
-
+        // Biding our view depending on the success
         task.addOnSuccessListener(k -> {
             museumBean = Objects.requireNonNull(task.getResult()).toObject(MuseumBean.class);
-            if (null != museumBean){
-                bindView(museumBean);
+            if (null != museumBean) {
                 Log.d(this.getClass().getName(), String.format("Document with id=%s fetched", DOCUMENT_ID));
+                bindView(museumBean);
+                // Starting our location manager
+                mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                refreshButton = findViewById(R.id.button);
+                refreshButton.setOnClickListener(click -> accessLocation());
             } else {
                 Toast.makeText(this, res.getString(R.string.document_is_null), Toast.LENGTH_LONG).show();
                 Log.e(this.getClass().getName(), String.format("Document with id=%s couldn't be parsed in java class", DOCUMENT_ID));
                 finish();
             }
-        });
-
-        task.addOnFailureListener(k -> {
+        }).addOnFailureListener(k -> {
             Toast.makeText(this, res.getString(R.string.unable_to_fetch), Toast.LENGTH_LONG).show();
             Log.e(this.getClass().getName(), String.format("Document with id=%s couldn't be fetched", DOCUMENT_ID));
             finish();
@@ -118,6 +146,11 @@ public class MuseumReaderActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Bind our view with the given bean
+     *
+     * @param museumBean museum bean to bind our view with
+     */
     private void bindView(final MuseumBean museumBean) {
         ((TextView) findViewById(R.id.tvMuseumName)).setText(museumBean.getNomDuMusee());
         ((TextView) findViewById(R.id.tvMuseumLocation)).setText(museumBean.getCompleteAdresse());
@@ -125,12 +158,23 @@ public class MuseumReaderActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.tvCurrDT)).setText(getDateTimeInstance().format(new Date()));
     }
 
-    public void bindDistanceToMuseum(String userPosition, MuseumBean museumBean){
+    /**
+     * Bind the informations related to the
+     * distance to museum in our view
+     *
+     * @param userPosition the user's position
+     * @param museumBean the museum
+     */
+    public void bindDistanceToMuseum(String userPosition, MuseumBean museumBean) {
 
         OkHttpClient okHttpClient = new OkHttpClient();
         TextView tvDistanceToMuseum = findViewById(R.id.tvDistanceToMuseum);
 
-        String json = String.format("{\"locations\":[[%s],[%s]],\"metrics\":[\"duration\",\"distance\"],\"units\":\"km\"}", userPosition, museumBean.getInvertedCoordonneesFinales());
+        String json = String.format(
+                "{\"locations\":[[%s],[%s]]," +
+                "\"metrics\":[\"duration\"," +
+                "\"distance\"],\"units\":\"km\"}",
+                userPosition, museumBean.getInvertedCoordonneesFinales());
 
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), json);
 
@@ -141,13 +185,16 @@ public class MuseumReaderActivity extends AppCompatActivity {
                 .post(requestBody)
                 .build();
 
-        Log.d(this.getClass().getName(), String.format("Following request has been sent : %s\n%s", request.toString(), json));
+        Log.d(this.getClass().getName(), String.format(
+                "Following request has been sent : %s\n%s",
+                request.toString(), json)
+        );
 
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Request request, IOException e) {
-                Log.w(this.getClass().getName(), "Request couldn't succeed");
-                tvDistanceToMuseum.setText(res.getString(R.string.error_remote_orss));
+                Log.e(this.getClass().getName(), "Request couldn't succeed");
+                runOnUiThread(() -> tvDistanceToMuseum.setText(res.getString(R.string.error_remote_orss)));
             }
 
             @RequiresApi(api = Build.VERSION_CODES.O)
@@ -156,9 +203,15 @@ public class MuseumReaderActivity extends AppCompatActivity {
                 String displayedMessage;
                 double durationToMuseum = -1;
                 double distanceToMuseum = -1;
-                if(!response.isSuccessful()) {
-                    Log.w(this.getClass().getName(), String.format("Request with error code : %s\n%s", response.code(), response.body().string()));
-                    displayedMessage = String.format("[%s] %s", response.code(), res.getString(R.string.error_remote_orss));
+                if (!response.isSuccessful()) {
+                    Log.w(this.getClass().getName(), String.format(
+                            "Request with error code : %s\n%s",
+                            response.code(), response.body().string())
+                    );
+                    displayedMessage = String.format(
+                            "[%s] %s",
+                            response.code(), res.getString(R.string.error_remote_orss)
+                    );
                 } else {
                     final JSONObject jsonReader;
                     try {
@@ -171,23 +224,26 @@ public class MuseumReaderActivity extends AppCompatActivity {
                         jsonArray = jsonReader.getJSONArray("distances");
                         distanceToMuseum = jsonArray.getJSONArray(0).getDouble(1);
 
-                        Log.w(this.getClass().getName(), String.format("Distance to museum : %s", durationToMuseum));
                     } catch (JSONException e) {
                         e.printStackTrace();
                     } finally {
-                        if(durationToMuseum == -1) {
-                            displayedMessage = "Erreur lors de l'affichage";
+                        if (durationToMuseum == -1 || distanceToMuseum == -1) {
+                            displayedMessage = res.getString(R.string.error_while_displaying);
+                            Log.e(this.getClass().getName(), displayedMessage);
                         } else {
-                            // Error, one hour of too much is displayed
-                            SimpleDateFormat formatter = new SimpleDateFormat("HH' heures 'mm' minutes 'ss' secondes'", Locale.FRANCE);
+                            SimpleDateFormat formatter = new SimpleDateFormat(
+                                    "HH' heures 'mm' minutes 'ss' secondes'", Locale.FRANCE
+                            );
                             formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
-                            displayedMessage = String.format("Distance jusqu'à la destination : %s km\nTemps jusqu'à la destination : %s", distanceToMuseum, formatter.format(new Date((long) durationToMuseum * 1000)));
+                            displayedMessage = String.format(
+                                    "Distance jusqu'à la destination : %s km\nTemps jusqu'à la destination : %s",
+                                    distanceToMuseum, formatter.format(new Date((long) durationToMuseum * 1000))
+                            );
+                            Log.d(this.getClass().getName(), String.format("Displayed message : %s", displayedMessage));
                         }
                     }
 
-                }
-                Log.d(this.getClass().getName(), String.format("Displayed message : %s", displayedMessage));
-                runOnUiThread(() -> tvDistanceToMuseum.setText(displayedMessage));
+                }runOnUiThread(() -> tvDistanceToMuseum.setText(displayedMessage));
             }
         });
     }
