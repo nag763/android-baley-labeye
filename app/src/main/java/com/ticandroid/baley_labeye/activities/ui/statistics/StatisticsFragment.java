6ee1331e2a57108a42298f215ab8c79ba73a0459
romain.baley <romain.baley@groupe-esigelec.org>
2020-12-22 17:29:45 +0100

    Statistiques

--- app/src/main/java/com/ticandroid/baley_labeye/activities/ui/statistics/StatisticsFragment.java
+++ app/src/main/java/com/ticandroid/baley_labeye/activities/ui/statistics/StatisticsFragment.java
@@ -2,20 +2,35 @@ package com.ticandroid.baley_labeye.activities.ui.statistics;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.ticandroid.baley_labeye.R;
 import com.ticandroid.baley_labeye.activities.ui.evaluer.EvaluerFragment;
 
 public class StatisticsFragment extends Fragment {
 
 
-    private transient TextView text;
+    private transient TextView title,nb,nombre,dist,distance;
+    private transient StorageReference stm;
+    private transient FirebaseAuth auth;
+    private transient int count=0;
+    private transient double distanceParcourue=0;
+
     public static Fragment newInstance() {
         return (new StatisticsFragment());
     }
@@ -28,7 +43,39 @@ public class StatisticsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_statistics, container, false);
-        text = root.findViewById(R.id.text);
+        title = root.findViewById(R.id.stat);
+        nb = root.findViewById(R.id.nb);
+        nombre = root.findViewById(R.id.nombre);
+        dist = root.findViewById(R.id.dist);
+        distance = root.findViewById(R.id.distance);
+        auth = FirebaseAuth.getInstance();
+        stm= FirebaseStorage.getInstance().getReference();
+        afficherNbMusees();
+
         return root;
+    }
+    private void afficherNbMusees(){
+
+        FirebaseFirestore.getInstance().collection("visites")
+                .whereEqualTo("idProfil",auth.getCurrentUser().getUid())
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    Log.d("count","count");
+                    for(QueryDocumentSnapshot doc : task.getResult()){
+                        Log.d("count"," "+count);
+                        count+=1;
+                        double d =(double) doc.get("distance");
+                       distanceParcourue = Double.sum(distanceParcourue,d);
+                       // distanceParcourue = distanceParcourue+doc.get("distance");
+                    }
+                    nombre.setText(""+count);
+                    distance.setText(""+distanceParcourue);
+                }
+            }
+        });
+
+
     }
 }
