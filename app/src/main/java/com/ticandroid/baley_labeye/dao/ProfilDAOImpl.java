c4f67ee4391f81f343719620febc9fd111c0ec89
nag763 <loic.labeye@tutanota.de>
2021-01-05 18:07:46 +0100

    Fix profilDAO issue

--- app/src/main/java/com/ticandroid/baley_labeye/dao/ProfilDAOImpl.java
+++ app/src/main/java/com/ticandroid/baley_labeye/dao/ProfilDAOImpl.java
@@ -1,22 +1,10 @@
 package com.ticandroid.baley_labeye.dao;
 
 import android.net.Uri;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.squareup.picasso.Picasso;
-import com.ticandroid.baley_labeye.activities.ProfilActivity;
-import com.ticandroid.baley_labeye.beans.ProfilBean;
-
-import java.io.File;
-import java.io.IOException;
 public class ProfilDAOImpl  {
     private StorageReference stm;
     private FirebaseAuth auth;
