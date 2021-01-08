646552b04bf407c4fe59fa7d89fed39fd7a15601
romain.baley <romain.baley@groupe-esigelec.org>
2021-01-08 12:32:31 +0100

    admin statistiques

--- app/src/main/java/com/ticandroid/baley_labeye/adapter/StatListAdapter.java
+++ app/src/main/java/com/ticandroid/baley_labeye/adapter/StatListAdapter.java
@@ -10,13 +10,27 @@ import androidx.annotation.NonNull;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.ticandroid.baley_labeye.R;
+import com.ticandroid.baley_labeye.beans.MuseumBean;
 import com.ticandroid.baley_labeye.beans.VisitBean;
 import com.ticandroid.baley_labeye.holder.MuseumListHolder;
 import com.ticandroid.baley_labeye.holder.StatListHolder;
 
-public class StatListAdapter extends FirestoreRecyclerAdapter<VisitBean, StatListHolder> {
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class StatListAdapter extends FirestoreRecyclerAdapter<MuseumBean, StatListHolder> {
     private transient final Context context;
+    //private transient CollectionReference visits;
+    private transient ArrayList<VisitBean> visitBeans;
+
+   // private transient VisitBean visitBean;
 
     /**
      * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
@@ -24,16 +38,45 @@ public class StatListAdapter extends FirestoreRecyclerAdapter<VisitBean, StatLis
      *
      * @param options
      */
-    public StatListAdapter(Context context,@NonNull FirestoreRecyclerOptions<VisitBean> options) {
+    public StatListAdapter(Context context,@NonNull FirestoreRecyclerOptions<MuseumBean> options) {
         super(options);
         this.context = context;
+        visitBeans = new ArrayList<>();
+        FirebaseFirestore.getInstance().collection("visites").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot doc : task.getResult()) {
+                        VisitBean visitBean = new VisitBean();
+                        visitBean.setNomDuMusee(doc.getString("nomDuMusee"));
+                        visitBean.setDistance( doc.getDouble("distance"));
+                        visitBean.setEvaluation( doc.getDouble("evaluation"));
+                        visitBeans.add(visitBean);
+                    }
+                }
+            }
+        });
+       // nbVisits=visitBeans.size();
+
+
     }
 
+
     @Override
-    protected void onBindViewHolder(@NonNull StatListHolder holder, int position, @NonNull VisitBean model) {
+    protected void onBindViewHolder(@NonNull StatListHolder holder, int position, @NonNull MuseumBean model) {
         final String TITLE = model.getNomDuMusee();
-        final double DISTANCE = model.getDistance();
-        final double EVALUATION = model.getEvaluation();
+        double DISTANCE=0;
+        double EVALUATION=0;
+        VisitBean[] visitsForMuseum = visitBeans.stream().filter(element -> element.getNomDuMusee().equals(model.getNomDuMusee())).toArray(VisitBean[]:: new);
+        if(visitsForMuseum.length!= 0){
+            for (VisitBean visit: visitsForMuseum) {
+                EVALUATION += visit.getEvaluation();
+                DISTANCE += visit.getDistance();
+            }
+            EVALUATION = EVALUATION/visitsForMuseum.length;
+            //nbVisits=visitsForMuseum.length;
+            //Log.d("visites",""+nbVisits);
+        }
         holder.setTextInTitleView(TITLE);
         holder.setTextInDistanceView(DISTANCE+"");
         holder.setTextInNoteView(EVALUATION+"");
--- app/src/main/java/com/ticandroid/baley_labeye/holder/StatListHolder.java
+++ app/src/main/java/com/ticandroid/baley_labeye/holder/StatListHolder.java
@@ -10,19 +10,19 @@ import com.ticandroid.baley_labeye.R;
 
 public class StatListHolder extends RecyclerView.ViewHolder{
     private transient final TextView title;
-    private transient final TextView numberVisits;
+    //private transient final TextView numberVisits;
     private transient final TextView note;
     private transient final TextView distance;
 
     private final static int CARD_TITLE = R.id.textTitle;
-    private final static int CARD_NUMBER_VISITS = R.id.textNumberVisits;
+   // private final static int CARD_NUMBER_VISITS = R.id.textNumberVisits;
     private final static int CARD_NOTE = R.id.textNote;
     private final static int CARD_DISTANCE = R.id.textDistance;
 
     public StatListHolder(@NonNull View itemView) {
         super(itemView);
         this.title = itemView.findViewById(CARD_TITLE);
-        this.numberVisits = itemView.findViewById(CARD_NUMBER_VISITS);
+        //this.numberVisits = itemView.findViewById(CARD_NUMBER_VISITS);
         this.note = itemView.findViewById(CARD_NOTE);
         this.distance = itemView.findViewById(CARD_DISTANCE);
     }
@@ -30,7 +30,7 @@ public class StatListHolder extends RecyclerView.ViewHolder{
     public void setTextInTitleView(String title){
         this.title.setText(title);
     }
-    public void setTextInNumberVisitsView(String numberVisits) { this.numberVisits.setText(numberVisits);}
+   // public void setTextInNumberVisitsView(String numberVisits) { this.numberVisits.setText(numberVisits);}
     public void setTextInNoteView(String note) { this.note.setText(note);}
     public void setTextInDistanceView(String distance) { this.distance.setText(distance);}
 }
